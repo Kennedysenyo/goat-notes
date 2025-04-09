a4988da008a09f4d24f73cdec384b9e6ca40be12
Kennedysenyo <kennedysenyo@gmail.com>
2025-04-09 09:49:24 +0000

    integrated supabase database

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { createClient } from "@/auth/server";
+import { prisma } from "@/db/prisma";
 import { handleError } from "@/lib/utils";
 
 
@@ -41,9 +42,16 @@ export async function signUpAction(email: string, password: string) {
     
     if (error) throw error;
 
-    if(!data.user?.id) throw new Error("Error Signing Up!")
+    const userId = data.user?.id
+    if(!userId) throw new Error("Error Signing Up!")
 
       // add user to database
+      await prisma.user.create({
+        data: {
+          id: userId,
+          email,
+        }
+      })
 
     return {errorMessage : null};
   }catch(error) {
